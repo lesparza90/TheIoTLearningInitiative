4fc9416c8e08dcdbd750c10339a5421b2126fab3
joseluis esparza <lesparza90@outlook.com>
2016-04-23 17:22:23 +0000

    actualizacion de main mosquito y flask

--- InternetOfThings101/main.py
+++ InternetOfThings101/main.py
@@ -1,5 +1,4 @@
-#!/usr/bin/python
-
+import paho.mqtt.client as paho
 import psutil
 import signal
 import sys
@@ -10,17 +9,35 @@ from threading import Thread
 def interruptHandler(signal, frame):
     sys.exit(0)
 
+def on_publish(mosq, obj, msg):
+    pass
+
 def dataNetwork():
     netdata = psutil.net_io_counters()
     return netdata.packets_sent + netdata.packets_recv
 
 def dataNetworkHandler():
-    idDevice = "IoT101Device"
+    idDevice = "ThisDevice"
+    mqttclient = paho.Client()
+    mqttclient.on_publish = on_publish
+    mqttclient.connect("test.mosquitto.org", 1883, 60)
     while True:
         packets = dataNetwork()
         message = idDevice + " " + str(packets)
         print "dataNetworkHandler " + message
+        mqttclient.publish("IoT101/Network", message)
         time.sleep(1)
+
+def on_message(mosq, obj, msg):
+    print "MQTT dataMessageHandler %s %s" % (msg.topic, msg.payload)
+
+def dataMessageHandler():
+    mqttclient = paho.Client()
+    mqttclient.on_message = on_message
+    mqttclient.connect("test.mosquitto.org", 1883, 60)
+    mqttclient.subscribe("IoT101/Message", 0)
+    while mqttclient.loop() == 0:
+        pass
 
 if __name__ == '__main__':
 
@@ -29,7 +46,9 @@ if __name__ == '__main__':
     threadx = Thread(target=dataNetworkHandler)
     threadx.start()
 
+    threadx = Thread(target=dataMessageHandler)
+    threadx.start()
+
     while True:
         print "Hello Internet of Things 101"
         time.sleep(5)
-
